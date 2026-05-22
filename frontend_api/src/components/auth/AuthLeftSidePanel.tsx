cfe4aa4b0874684d5a39b4d2afbab67a1e7dfd79
Deepak Joshi <vaporuser69@gmail.com>
2026-05-22 15:17:48 +0545

    responsive deisgn layout

--- frontend_api/src/components/auth/AuthLeftSidePanel.tsx
+++ frontend_api/src/components/auth/AuthLeftSidePanel.tsx
@@ -2,16 +2,16 @@ import { LogoComponent } from "../LogoComponent";
 
 export default function AuthLeftSidePanel() {
   return (
-    <>
-      <div className="flex w-1/4 h-screen ">
-        <div className="w-full bg-gradient-secondary flex flex-col items-center justify-center">
-          <LogoComponent className="bg-transparent/50 backdrop-blur-sm p-6 rounded-xl size-38" />
-          <div className="text-center font-semibold text-primary-950">
-            <h3>Buy Whatever You Want !!</h3>
-          </div>
-        </div>
-        {/* <div className="w-full lg:w-2/3 bg-gray-200"></div> */}
+    <div className="w-72 md:w-80 h-screen bg-gradient-secondary flex flex-col items-center justify-center px-6">
+      
+      <LogoComponent className="bg-transparent/50 backdrop-blur-sm p-4 sm:p-6 rounded-xl w-32 h-32 sm:w-40 sm:h-40" />
+
+      <div className="text-center font-semibold text-primary-950 mt-6">
+        <h3 className="text-lg sm:text-xl">
+          Buy Whatever You Want !!
+        </h3>
+
       </div>
-    </>
+    </div>
   );
 }
